d9ab9647c472dd41d52137256a842313c950f0e8
PhanNgocLuan <luanphan110@gmail.com>
2021-11-03 21:02:15 +0700

    commit slide

---
+++ app/Type.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    protected $table = "types";
+    //
+    public function protype() {
+        return $this->hasMany('App\Protype','type_id','type_id');
+    }
+    //
+    public function getTypeNameById($type_id) {
+        $type = DB::table('types')->where('type_id','=',$type_id)->first();
+        return $type;
+    }
+}
