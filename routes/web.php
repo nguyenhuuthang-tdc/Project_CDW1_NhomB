7d0e8d6a79e84aa6e1dc4b59e6382cb98e134523
nguyenhuuthang-tdc <nguyenhuuthang12c8@gmail.com>
2021-11-07 10:20:41 +0700

    refresh vendor

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
