bd98c92d64572c31ded96887fccbf80fea29f65c
nguyenhuuthang-tdc <nguyenhuuthang12c8@gmail.com>
2021-11-07 10:14:11 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
+
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
